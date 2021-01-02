0d243546429687cb06eee41596a67a5d0bb4e8af
afyavuz53 <76837995+afyavuz53@users.noreply.github.com>
2021-01-02 17:10:57 +0300

    second commit

---
+++ ImageHelper.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArtworkForShare.UI.MVC.Helper
+{
+    public static class ImageHelper
+    {
+        [Obsolete]
+        public static string GetImageUrl(IFormFile file, IHostingEnvironment environment)
+        {
+            string filePath = Path.Combine(environment.WebRootPath, "Content/Images");//../wwwroot/content/images
+            string newName = string.Empty;
+            if (file != null)
+            {
+                string dosya = file.FileName;
+                string[] dosyaArray = dosya.Split('.');//görselin uzantısını yakalamak için dosya adı noktaya(.) göre iki diziye ayrılır. 
+                newName = Guid.NewGuid().ToString() + "." + dosyaArray[dosyaArray.Length - 1];//resme rasgele ad verilerek yeni adı uzantısı ile beraber atanır.
+
+                using (var fileStream = new FileStream(Path.Combine(filePath, newName), FileMode.Create))//görseli belirtilen dosya yoluna yeni adı ile kaydeder.
+                {
+                    file.CopyTo(fileStream);
+                }
+            }
+            return "../../Content/Images/"+newName;//"../../Content/Images/[dosya adı].[uzantı]"
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Helpers
  Projelerde kullanılabilecek yararlı yardımcı sınıflar.
+ 
+ 1.PasswordHelper: Veri tabanında şifreleri daha güvenli birşekilde saklamak için salting (tuzlama) yöntemini sağlayan helper.
+
+ 2.ImageHelper: Resimleri belirtilen dosya yoluna rasgele isimlendirerek kaydeder ve geriye dosyanın yeni adıyla birleştirilmiş dosya yolunu döner.
