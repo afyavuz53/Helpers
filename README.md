049f7ea3de3388cbb38696b33697576311f021d0
afyavuz53 <76837995+afyavuz53@users.noreply.github.com>
2021-01-26 21:13:04 +0300

    UploadImg

--- README.md
+++ README.md
@@ -4,3 +4,7 @@
  1.PasswordHelper: Veri tabanında şifreleri daha güvenli birşekilde saklamak için salting (tuzlama) yöntemini sağlayan helper.
 
  2.ImageHelper: Resimleri belirtilen dosya yoluna rasgele isimlendirerek kaydeder ve geriye dosyanın yeni adıyla birleştirilmiş dosya yolunu döner.
+
+ 3.MailHelper: Mail göndermek için kullanılabilir.
+
+ 4.UploadImage: https://cloudinary.com sitesinin API servisini kullanarak siteye görsel depoloma.
---
+++ UploadImage.cs
@@ -0,0 +1,32 @@
+﻿using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ava.UI.WinForm.Helper
+{
+    static class UploadImage
+    {
+        //https://cloudinary.com sitesini api servisini kullanarak görsel yükleme
+        const string cloud_name = "......";                 //  Sitede açtığınızda
+        const string api_key = "......";                    //  bu bilgileri 
+        const string api_secret = "..........";             //  DashBoard ==>> Account Details de görebilirsiniz.
+
+        [Obsolete]
+        public static string UploadImageToCloudinary(string imagePath)//OpenFileDialog'dan çektiğiniz dosya yolu gönderilir...
+        {
+            Account account = new Account(cloud_name, api_key, api_secret);
+            Cloudinary cloudinary = new Cloudinary(account);
+            
+            var uploadParams = new ImageUploadParams()
+            {
+                File = new FileDescription(imagePath)
+            };                        
+            uploadParams.File.FileName = "[belgeadı]" //    isteğe bağlı yüklenecek görselin adı değiştirilebilir ya da 
+            uploadParams.Folder = "[klasör adı]"      //    isteğe bağlı site içersinde klasöre yüklemek istediğinizde doldurabilirsiniz.
+            var uploadResult = cloudinary.Upload(uploadParams); //    görselin yüklendiği url'i elde edebilirsiniz.
+            return uploadResult.Uri.AbsoluteUri;      
+        }
+    }
+}
